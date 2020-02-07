b3df131411d2b3332fe35aba1241b31acc918316
Felipe Sammarco <felipesammarco@gmail.com>
2020-02-06 22:27:55 -0300

    nova versão

---
+++ segundo.php
@@ -0,0 +1,40 @@
+aas
+addslashes
+
+addslashes
+
+sapi_windows_cp_convv
+<var>cxv
+
+
+<dfn>dba_syncfs
+
+
+fsd
+fscanf
+dns_get_mxg
+dfg
+<dfn>gc_collect_cyclesfg
+    <s>fscanf
+        fscanfe
+        rad2degsf
+        shm_detachf
+        spl_autoload_functionss
+        disk_free_spacev
+        xml_get_current_byte_indexvx
+        curl_version
+
+
+        vxcvxcv
+
+
+
+    fsfds
+
+spl_autoload_functions
+
+
+    </s>
+</dfn>
+</dfn>
+</var>
